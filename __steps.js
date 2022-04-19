92e89c3dd2dc00a89ffbfebb81cd67fbb4b44227
sarikamahboob <sarika.mahboob1234@gmail.com>
2022-04-20 02:10:31 +0600

    added steps

---
+++ __steps.js
@@ -0,0 +1,26 @@
+/* 
+-----------------------------
+Basic Server Setup
+1. create a folder (manually or mkdir)
+2. open command line to that folder
+3. npm init
+4. npm install express
+5. create index.js
+
+-----------------------
+for express / node server
+-----------------------
+1. require express
+2. create ap variable
+3. declare port
+4. set app.get('/')
+6. node index.js
+7. check your browser for that port
+-------------
+get
+-------------
+1. cors middleware
+--------------------------
+post
+--------------------------
+*/
